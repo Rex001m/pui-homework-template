e0d45b39b5e7f516048e5762017b6001cf362627
Rex001m <kema@andrew.cmu.edu>
2024-09-29 23:19:06 -0400

    final

--- solution-hw4/random-element.js
+++ solution-hw4/random-element.js
@@ -1,34 +1,4 @@
-//hw4 starts
 
-const cart= []
-
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const rollType = params.get('roll');
-
-// //try figuring out the extract info from the data base
-// //method 1
-// class Newpage {
-
-//     constructor(name,price,imagePath) {
-//       this.pageName = name;
-//       this.pagePrice = price;
-//       this.pageImagePath = imagePath;
-//     }
-
-// }
-// //method 2
-// function getCurrentRollInfo() {
-//     const currentRollName = rollNames[currentRollIndex]; // Get the current roll's name
-//     const currentRollData = rolls[currentRollName]; // Access the roll data using the name
-    
-//     // Extract name, price, and image path
-//     const name = currentRollName;
-//     const price = currentRollData.basePrice;
-//     const imagePath = currentRollData.imageFile;
-// }
-
-//hw4 end
 //Replace selection box
 const glazingOptionsJava = ['Keep original', 'Sugar milk', 'Vanilla milk', 'Double chocolate'];
 const packSizeOptionsJava = ['1','3','6','12'];
@@ -85,7 +55,7 @@ function calculatePrice (){
     const packPrice =packSize[selectedQuantity];
     const basePrice = 2.49;
     finalPrice=(basePrice + glazingPrice)*packPrice
-    display.textContent = `$${finalPrice.toFixed(2)} `;
+   // display.textContent = `$${finalPrice.toFixed(2)} `;
 }
 
 glazingDropd.addEventListener("change",onDropDownChange);
