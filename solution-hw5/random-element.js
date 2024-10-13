780bf7e15e72469376ecc37dd4b0c43815dae2d7
Rex001m <kema@andrew.cmu.edu>
2024-10-13 18:55:39 -0400

    hw5 finish

--- solution-hw5/random-element.js
+++ solution-hw5/random-element.js
@@ -14,82 +14,82 @@ const packSize= {
 
 
 //just for detail page func
-if (window.location.pathname.includes('/Productdetial.html')){
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const rollType = params.get('roll');
+// if (window.location.pathname.includes('/Productdetial.html')){
+// const queryString = window.location.search;
+// const params = new URLSearchParams(queryString);
+// const rollType = params.get('roll');
 
 
-// Update the tilte
-const detailName = document.querySelector(".title");
-detailName.innerText = rollType + ' Cinnamon Roll';
+// // Update the tilte
+// const detailName = document.querySelector(".title");
+// detailName.innerText = rollType + ' Cinnamon Roll';
 
 
-// Update the image
-const detailImage = document.querySelector(".galleryimageD");
-detailImage.src =  '../assets/products/'+ rolls[rollType].imageFile;
+// // Update the image
+// const detailImage = document.querySelector(".galleryimageD");
+// detailImage.src =  '../assets/products/'+ rolls[rollType].imageFile;
 
-// Update the Price
-const detailPrice = document.querySelector("#display");
-detailPrice.innerText = "$" +rolls[rollType].basePrice;
+// // Update the Price
+// const detailPrice = document.querySelector("#display");
+// detailPrice.innerText = "$" +rolls[rollType].basePrice;
 
-//Replace selection box
-const glazingOptionsJava = ['Keep original', 'Sugar milk', 'Vanilla milk', 'Double chocolate'];
-const packSizeOptionsJava = ['1','3','6','12'];
+// //Replace selection box
+// const glazingOptionsJava = ['Keep original', 'Sugar milk', 'Vanilla milk', 'Double chocolate'];
+// const packSizeOptionsJava = ['1','3','6','12'];
 
-function populate(){
-  const glazingSelect = document.querySelector('#glazingOptions');
+// function populate(){
+//   const glazingSelect = document.querySelector('#glazingOptions');
   
-  for (let i = 0; i < glazingOptionsJava.length; i=i+1) {
-      const option = document.createElement('option');
-      option.textContent = glazingOptionsJava[i];
-      glazingSelect.add(option);
-  }
+//   for (let i = 0; i < glazingOptionsJava.length; i=i+1) {
+//       const option = document.createElement('option');
+//       option.textContent = glazingOptionsJava[i];
+//       glazingSelect.add(option);
+//   }
   
-  const sizeSelect = document.querySelector('#packSizeOptions');
-  for (let i = 0; i < packSizeOptionsJava.length; i=i+1) {
-      const option = document.createElement('option');
-      option.textContent = packSizeOptionsJava[i];
-      sizeSelect.add(option);
-  }
-  }
+//   const sizeSelect = document.querySelector('#packSizeOptions');
+//   for (let i = 0; i < packSizeOptionsJava.length; i=i+1) {
+//       const option = document.createElement('option');
+//       option.textContent = packSizeOptionsJava[i];
+//       sizeSelect.add(option);
+//   }
+//   }
   
   
-  populate();
+//   populate();
 
-let glazingDropd = document.querySelector('#glazingOptions');
-let packSizeDropd = document.querySelector('#packSizeOptions');
-let price = document.querySelector('#display');
-let selectedGlaze = glazingDropd.value;
-let selectedQuantity = packSizeDropd.value;
-
-
-function onDropDownChange(){
-  selectedGlaze=glazingDropd.value;
-  selectedQuantity=packSizeDropd.value;
-  calculatePrice();
-}
-
-function calculatePrice (){
-  const glazingPrice = glazing[selectedGlaze];
-  const packPrice =packSize[selectedQuantity];
-  const basePrice = Number (rolls[rollType].basePrice);
-  finalPrice=(basePrice + glazingPrice)*packPrice
-  price.textContent = `$${finalPrice.toFixed(2)}`;
-}
+// let glazingDropd = document.querySelector('#glazingOptions');
+// let packSizeDropd = document.querySelector('#packSizeOptions');
+// let price = document.querySelector('#display');
+// let selectedGlaze = glazingDropd.value;
+// let selectedQuantity = packSizeDropd.value;
 
 
-glazingDropd.addEventListener("change",onDropDownChange);
-packSizeDropd.addEventListener("change", onDropDownChange)
-calculatePrice();
+// function onDropDownChange(){
+//   selectedGlaze=glazingDropd.value;
+//   selectedQuantity=packSizeDropd.value;
+//   calculatePrice();
+// }
+
+// function calculatePrice (){
+//   const glazingPrice = glazing[selectedGlaze];
+//   const packPrice =packSize[selectedQuantity];
+//   const basePrice = Number (rolls[rollType].basePrice);
+//   finalPrice=(basePrice + glazingPrice)*packPrice
+//   price.textContent = `$${finalPrice.toFixed(2)}`;
+// }
+
+
+// glazingDropd.addEventListener("change",onDropDownChange);
+// packSizeDropd.addEventListener("change", onDropDownChange)
+// calculatePrice();
 
 
 
-document.querySelector("#addtocartbutton").addEventListener('click',function(){
-  let roll =new Roll(rollType,selectedGlaze,selectedQuantity,rolls[rollType].basePrice)
-  cart.push(roll)
-  })
-}
+// document.querySelector("#addtocartbutton").addEventListener('click',function(){
+//   let roll =new Roll(rollType,selectedGlaze,selectedQuantity,rolls[rollType].basePrice)
+//   cart.push(roll)
+//   })
+// }
 
 // //Replace selection box
 // const glazingOptionsJava = ['Keep original', 'Sugar milk', 'Vanilla milk', 'Double chocolate'];
