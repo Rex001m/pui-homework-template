2dad4dc1888d8d382e723ea9c0cec150f0528c52
Rex001m <kema@andrew.cmu.edu>
2024-11-10 23:42:58 -0500

    first draft

--- Final-project/script.js
+++ Final-project/script.js
@@ -1,22 +1,21 @@
-// script.js
 
-// Function to simulate the loading progress
-let currentProgress = 0;
-let loadingText = document.getElementById("loading-text");
-let loadingScreen = document.getElementById("loading-screen");
-let mainContent = document.getElementById("main-content");
 
-// Function to update the loading screen
-function updateLoadingScreen() {
-  if (currentProgress <= 100) {
-    loadingText.textContent = currentProgress + "%";
-    currentProgress++;
-  } else {
-    // Hide loading screen and show the main content
-    loadingScreen.style.display = "none";
-    mainContent.style.display = "block";
+  // Function to simulate the loading progress
+  let currentProgress = 0;
+  let loadingText = document.getElementById("loading-text");
+  let loadingScreen = document.getElementById("loading-screen");
+  let startIcon = document.getElementById("startIcon");
+
+  // Function loading screen
+  function updateLoadingScreen() {
+    if (currentProgress <= 100) {
+      loadingText.textContent = currentProgress + "%";
+      currentProgress++;
+    } else {
+      // Hide loading screen and show the main content
+      loadingScreen.style.display = "none";
+      startIcon.style.display = "block";
+    }
   }
-}
-
-// Call the updateLoadingScreen function every 50ms to simulate loading progress
-let loadingInterval = setInterval(updateLoadingScreen, 50);
+  //Update speed
+  let loadingInterval = setInterval(updateLoadingScreen, 20);
