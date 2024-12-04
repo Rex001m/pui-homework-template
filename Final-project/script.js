1da560f153e98666474cd67ead33272b3b72cc7d
Rex001m <kema@andrew.cmu.edu>
2024-12-04 09:40:23 -0500

    gsap update

--- Final-project/script.js
+++ Final-project/script.js
@@ -23,3 +23,20 @@ if (window.location.href.endsWith('index.html')) {
   let loadingInterval = setInterval(updateLoadingScreen, 20);
 }
 //Import GSAP library
+
+// Hover animation using GSAP
+mainVision.addEventListener("mouseenter", () => {
+  gsap.to(mainVision, {
+    scale: 1.2, 
+    duration: 0.3, 
+    ease: "power2.out" 
+  });
+});
+
+mainVision.addEventListener("mouseleave", () => {
+  gsap.to(mainVision, {
+    scale: 1, 
+    duration: 0.3,
+    ease: "power2.in"
+  });
+});
