ec204bc41bf51b3449b03b2aac2ef2c63426af82
Rex001m <kema@andrew.cmu.edu>
2024-12-05 13:44:42 -0500

    link fix

---
+++ Final-project/Loading.js
@@ -0,0 +1,19 @@
+// Function to simulate the loading progress
+let currentProgress = 0;
+let loadingText = document.getElementById("loading-text");
+let loadingScreen = document.getElementById("loading-screen");
+let startIcon = document.getElementById("startIcon");
+
+// Function loading screen
+function updateLoadingScreen() {
+  if (currentProgress <= 100) {
+    loadingText.textContent = currentProgress + "%";
+    currentProgress++;
+  } else {
+    // Hide loading screen and show the main content
+    loadingScreen.style.display = "none";
+    startIcon.style.display = "block";
+  }
+}
+//Update speed
+let loadingInterval = setInterval(updateLoadingScreen, 20);
--- Final-project/script.js
+++ Final-project/script.js
@@ -1,27 +1,27 @@
-// Check if the current URL matches the target URL
-if (window.location.href.endsWith('index.html')) {
-  console.log(window.location.href)
+// // Check if the current URL matches the target URL
+// if (window.location.href.endsWith('index.html')) {
+//   console.log(window.location.href)
 
-  // Function to simulate the loading progress
-  let currentProgress = 0;
-  let loadingText = document.getElementById("loading-text");
-  let loadingScreen = document.getElementById("loading-screen");
-  let startIcon = document.getElementById("startIcon");
+//   // Function to simulate the loading progress
+//   let currentProgress = 0;
+//   let loadingText = document.getElementById("loading-text");
+//   let loadingScreen = document.getElementById("loading-screen");
+//   let startIcon = document.getElementById("startIcon");
 
-  // Function loading screen
-  function updateLoadingScreen() {
-    if (currentProgress <= 100) {
-      loadingText.textContent = currentProgress + "%";
-      currentProgress++;
-    } else {
-      // Hide loading screen and show the main content
-      loadingScreen.style.display = "none";
-      startIcon.style.display = "block";
-    }
-  }
-  //Update speed
-  let loadingInterval = setInterval(updateLoadingScreen, 20);
-}
+//   // Function loading screen
+//   function updateLoadingScreen() {
+//     if (currentProgress <= 100) {
+//       loadingText.textContent = currentProgress + "%";
+//       currentProgress++;
+//     } else {
+//       // Hide loading screen and show the main content
+//       loadingScreen.style.display = "none";
+//       startIcon.style.display = "block";
+//     }
+//   }
+//   //Update speed
+//   let loadingInterval = setInterval(updateLoadingScreen, 20);
+// }
 
 
 // Hover interaction for profileDescription
